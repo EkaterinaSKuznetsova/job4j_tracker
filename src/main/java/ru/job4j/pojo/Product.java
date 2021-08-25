7c58d736c33a94f804b4d56c7e732b46b7ac9f36
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-08-25 21:22:51 +0300

    4. Сравнение моделей. Метод равен [# 174893]

--- src/main/java/ru/job4j/pojo/Product.java
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -25,5 +25,22 @@ public class Product {
     public void setCount(int count) {
         this.count = count;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return count == product.count && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, count);
+    }
 }
 

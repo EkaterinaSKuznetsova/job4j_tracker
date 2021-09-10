e4ea82f5d036f0d45643da2d13cdfa605675c40c
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-09-10 19:39:01 +0300

    9.2. Тесты вывода на консоль [#33585 #200728]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -52,7 +52,7 @@ public class Item {
     public String toString() {
         return "Item{" + "id=" + id 
                 + ", name='" + name + '\''
-                + ", created=" + created    //.format(FORMATTER)
+                + ", created=" + created.format(FORMATTER)
                 + '}';
     }
 }

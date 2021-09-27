5fd24dda0457a70cc1362cf98df198578fb04162
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-09-27 20:15:57 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094 #209222]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<String>();
         arrayList.add("Petr");
         arrayList.add("Ivan");
         arrayList.add("Stepan");

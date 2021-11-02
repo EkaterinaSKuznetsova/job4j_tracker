750df5f5053571c703ed00443d47390e73d936ba
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-11-02 11:43:49 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lamda/LambdaUsage.java
@@ -0,0 +1,29 @@
+package ru.job4j.lamda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        String[] numbers = {
+                "f",
+                "jhjhjkh",
+                "ghghgglkgkgglg",
+                "fhf",
+                "jj"
+        };
+        System.out.println("Array before compare:");
+        for (String number : numbers) {
+            System.out.println(number);
+        }
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+        Arrays.sort(numbers, comparator);
+        System.out.println("Array after compare:");
+        for (String number : numbers) {
+            System.out.println(number);
+        }
+    }
+}

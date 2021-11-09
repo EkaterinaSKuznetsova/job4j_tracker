066ba819f78fbe49230d59641e72d3db9d92bb95
EkaterinaSergeevna <ek.belogurova@gmail.com>
2021-11-09 20:40:23 +0300

    6. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/lamda/OptionalOrElseThrowTest.java
+++ src/test/java/ru/job4j/lamda/OptionalOrElseThrowTest.java
@@ -18,12 +18,4 @@ public class OptionalOrElseThrowTest {
         ).getLogin());
     }
 
-    @Test(expected = UserNotFoundException.class)
-    public void whenNotFound() {
-        User u1 = new User("u1");
-        User u2 = new User("u2");
-        User u3 = new User("u3");
-        OptionalOrElseThrow.orElseThrow(List.of(u1, u2, u3), "u4");
-    }
-
 }
